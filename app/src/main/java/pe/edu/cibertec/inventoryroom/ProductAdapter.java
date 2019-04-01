0ef9a0b6e636ac0d5f7e4f70af76b4e898263832
Mayumi <cmta2112@gmail.com>
2019-03-31 19:09:12 -0500

    InventoryRoom

--- app/src/main/java/pe/edu/cibertec/inventoryroom/ProductAdapter.java
+++ app/src/main/java/pe/edu/cibertec/inventoryroom/ProductAdapter.java
@@ -7,12 +7,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductPrototype> {
 
-    private List    <Product> items;
+    private List<Product> items;
 
     public ProductAdapter(List<Product> items) {
         this.items = items;
